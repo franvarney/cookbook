2ce72065fd6b5e2a4dda8c92183e2253a0ec1d62
Fran Varney <fran.varn@gmail.com>
2015-08-07 12:58:16 +0000

    Add JS and CSS

---
+++ resources/assets/js/app.js
@@ -0,0 +1,74 @@
+Recipe = (function () {
+  var el = {};
+  return {
+    init: function () {
+      el.dirButton  = $('#add-direction');
+      el.dirUl      = $('#add-directions');
+      el.dirLast    = el.dirUl.children().last();
+      el.dirRemove  = $('.remove-direction');
+      el.ingButton  = $('#add-ingredient');
+      el.ingUl      = $('#add-ingredients');
+      el.ingLast    = el.ingUl.children().last();
+      el.ingRemove  = $('.remove-ingredient');
+      //el.recipeForm = $('.recipe-form');
+      this.addDirection();
+      this.addIngredient();
+      this.removeDirection();
+      this.removeIngredient();
+      //this.submitRecipe();
+    },
+    addDirection: function () {
+      el.dirButton.click(function (e) {
+        e.preventDefault();
+        var li = el.dirLast.clone(true);
+        $.each(li.children(), function (index, child) {
+          child.value = null;
+        });
+        el.dirUl.append(li);
+      });
+    },
+    addIngredient: function () {
+      el.ingButton.click(function (e) {
+        e.preventDefault();
+        var li = el.ingLast.clone(true);
+        $.each(li.children(), function (index, child) {
+          child.value = null;
+        });
+        el.ingUl.append(li);
+      });
+    },
+    removeDirection: function () {
+      el.dirRemove.click(function (e) {
+        e.preventDefault();
+        $(e.target).parent().remove();
+      });
+    },
+    removeIngredient: function () {
+      el.ingRemove.click(function (e) {
+        e.preventDefault();
+        $(e.target).parent().remove();
+      });
+    },
+    submitRecipe: function () {
+      // el.recipeForm.submit(function (e) {
+      //   $.ajax({
+      //       type: el.recipeForm.attr('method'),
+      //       url: el.recipeForm.attr('action'),
+      //       data: el.recipeForm.serialize(),
+      //       success: function (data, textStatus, jqXHR) {
+      //         console.log(data);
+      //         window.location.href = '/recipe/' + data.recipe_id;
+      //       },
+      //       error: function (err) {
+      //         console.log(err);
+      //       }
+      //   });
+      //   e.preventDefault();
+      // });
+    }
+  }
+})();
+
+$(document).ready(function() {
+  Recipe.init();
+});
