e6e5758744fbb854a927198550b5fef6512f7d7f
Fran Varney <fran.varn@gmail.com>
2015-08-23 03:27:50 +0000

    Change name and add/change urls

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -7,7 +7,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">Digipes</a>
+      <a class="navbar-brand" href="#">Snibbles</a>
     </div>
 
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
@@ -20,10 +20,13 @@
           <li><a href="{{ url('/login') }}">Login</a></li>
           <li><a href="{{ url('/register') }}">Register</a></li>
         @else
+          <li><a href="{{ url('/cookbook/create') }}">New Cookbook</a></li>
+          <li><a href="{{ url('/recipe/create') }}">New Recipe</a></li>
           <li class="dropdown">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->username }} <span class="caret"></span></a>
             <ul class="dropdown-menu" role="menu">
-              <li><a href="{{ url('/cookbooks') }}">Cookbooks</a></li>
+              <li><a href="{{ url('/user/' . Auth::user()->id . '/cookbooks') }}">Cookbooks</a></li>
+              <li><a href="{{ url('/requests/' . Auth::user()->id ) }}">Requests</a></li>
               <li><a href="{{ url('/account') }}">Account</a></li>
               <li><a href="{{ url('/logout') }}">Logout</a></li>
             </ul>
