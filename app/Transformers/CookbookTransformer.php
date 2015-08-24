0d6454035a68eb59344a34a4c2e7f44419261953
Fran Varney <fran.varn@gmail.com>
2015-08-24 12:28:54 +0000

    Add id, reorder use statements, and reformat return block

--- app/Transformers/CookbookTransformer.php
+++ app/Transformers/CookbookTransformer.php
@@ -1,9 +1,9 @@
 <?php namespace App\Transformers;
 
 use App\Models\Cookbook;
-use App\Transformers\UserTransformer;
 use League\Fractal\Manager;
 use League\Fractal\Resource\Item;
+use App\Transformers\UserTransformer;
 use League\Fractal\TransformerAbstract;
 
 class CookbookTransformer extends TransformerAbstract
@@ -23,13 +23,14 @@ class CookbookTransformer extends TransformerAbstract
 		$user = $this->manager->createData($item)->toArray();
 
 		return [
-				'name' => $cookbook->name,
-				'description' => $cookbook->description,
-				'creator' => $user['data'],
-				'url' => '/cookbook/' . $cookbook->id,
-				'public' => (bool) $cookbook->is_public,
-				'created' => date('m/d/y', strtotime($cookbook->created_at)),
-				'updated' => date('m/d/y', strtotime($cookbook->updated_at))
-			];
+			'id' => $cookbook->id,
+			'name' => $cookbook->name,
+			'description' => $cookbook->description,
+			'creator' => $user['data'],
+			'url' => '/cookbook/' . $cookbook->id,
+			'public' => (bool) $cookbook->is_public,
+			'created' => date('m/d/y', strtotime($cookbook->created_at)),
+			'updated' => date('m/d/y', strtotime($cookbook->updated_at))
+		];
 	}
 }
