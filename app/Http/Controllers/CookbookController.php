2666dc6108f87f7c8612b76bc00beeb2b25dddd4
Fran Varney <fran.varn@gmail.com>
2015-08-24 12:55:22 +0000

    Add various requests

--- app/Http/Controllers/CookbookController.php
+++ app/Http/Controllers/CookbookController.php
@@ -1,18 +1,37 @@
-<?php
-
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
+<?php namespace App\Http\Controllers;
 
 use App\Http\Requests;
-use App\Http\Controllers\Controller;
 use GuzzleHttp\Client;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class CookbookController extends Controller
 {
-	public function __construct(Client $client)
+	public function __construct()
+    {
+        $this->client = new \GuzzleHttp\Client();
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+	public function index($user_id)
 	{
-		$this->client = $client;
+		$contents = new \StdClass;
+
+		$cookbook_response = $this->client->get(env('API_URL') . '/cookbooks/' . $user_id . '/contributor');
+		$contents->cookbooks = json_decode($cookbook_response->getBody())->cookbooks;
+
+		$contributors = (string) NULL;
+		foreach($contents->cookbooks as $key => $cookbook) {
+			$contributor_response = $this->client->get(env('API_URL') . '/contributors/' . $cookbook->id . '/cookbook');
+			$contributors[] = json_decode($contributor_response->getBody())->contributors;
+		}
+		$contents->contributors = $contributors;
+
+		return view('cookbooks.index', ['contents' => $contents]);
 	}
 
 	/**
@@ -33,7 +52,8 @@ class CookbookController extends Controller
 	 */
 	public function store(Request $request)
 	{
-		// $response = $this->client->post(env('API_URL') . '/cookbook', ['json' => $request]);
+		$response = $this->client->post(env('API_URL') . '/cookbook', ['form_params' => $request->all()]);
+        return redirect('/cookbook/' . json_decode($response->getBody())->cookbook_id);
 	}
 
 	/**
@@ -44,9 +64,9 @@ class CookbookController extends Controller
 	 */
 	public function show($id)
 	{
-		$contents = new \StdClass;
-
 		$cookbook_response = $this->client->get(env('API_URL') . '/cookbook/' . $id);
+
+		$contents = new \StdClass;
 		$contents->cookbook = json_decode($cookbook_response->getBody())->cookbook[0];
 
 		$recipe_response = $this->client->get(env('API_URL') . '/recipes/' . $id . '/cookbook');
@@ -63,7 +83,12 @@ class CookbookController extends Controller
 	 */
 	public function edit($id)
 	{
-		//
+		$response = $this->client->get(env('API_URL') . '/cookbook/' . $id);
+
+		$contents = new \StdClass;
+		$contents->cookbook = json_decode($response->getBody())->cookbook[0];
+
+		return view('cookbooks.edit', ['contents' => $contents]);
 	}
 
 	/**
@@ -75,7 +100,8 @@ class CookbookController extends Controller
 	 */
 	public function update(Request $request, $id)
 	{
-		//
+		$response = $this->client->put(env('API_URL') . '/cookbook/' . $id, ['form_params' => $request->all()]);
+		return redirect('/cookbook/' . $id);
 	}
 
 	/**
@@ -86,6 +112,7 @@ class CookbookController extends Controller
 	 */
 	public function destroy($id)
 	{
-		//
+		$response = $this->client->delete(env('API_URL') . '/cookbook/' . $id);
+		return redirect('/');
 	}
 }
