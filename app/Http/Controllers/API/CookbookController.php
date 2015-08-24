a4548f15ec1f926d544ae3e057e5984afd0a1a3d
Fran Varney <fran.varn@gmail.com>
2015-08-24 12:54:32 +0000

    Remove repo, add/fix code accordingly, and reorder use statements

--- app/Http/Controllers/API/CookbookController.php
+++ app/Http/Controllers/API/CookbookController.php
@@ -1,31 +1,49 @@
 <?php namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
-use App\Repositories\CookbookRepository;
-use App\Transformers\CookbookTransformer;
-use Illuminate\Http\Request;
+use App\Models\Cookbook;
+use App\Models\Contributor;
 use League\Fractal\Manager;
-use League\Fractal\Resource\Collection;
+use Illuminate\Http\Request;
 use League\Fractal\Resource\Item;
+use App\Http\Controllers\Controller;
+use League\Fractal\Resource\Collection;
+use App\Transformers\CookbookTransformer;
 use League\Fractal\Serializer\JsonApiSerializer;
 
 class CookbookController extends Controller
 {
 	public function __construct(
-			CookbookRepository $cookbook_repo,
 			CookbookTransformer $cookbook_transformer,
 			Manager $manager
 		)
 	{
-		$this->cookbook = $cookbook_repo;
 		$this->transformer = $cookbook_transformer;
 		$this->manager = $manager->setSerializer(new JsonApiSerializer());
 	}
 
+	public function allIfContributor($user_id)
+	{
+		$contributor_cookbook_ids = Contributor::where(['user_id' => $user_id])->lists('cookbook_id');
+		$cookbooks = Cookbook::whereIn('id', $contributor_cookbook_ids)->get();
+
+		$items = new Collection($cookbooks, $this->transformer, 'cookbooks');
+		$data = $this->manager->createData($items)->toArray();
+
+		return response()->json($data, 201);
+	}
+
+	public function allIfContributorList($user_id)
+	{
+		$contributor_cookbook_ids = Contributor::where(['user_id' => $user_id])->lists('cookbook_id');
+		$cookbooks = Cookbook::whereIn('id', $contributor_cookbook_ids)->lists('name', 'id');
+
+		return response()->json($cookbooks, 201);
+	}
+
 	public function allByUserId($user_id)
 	{
-		$cookbooks = $this->cookbook->allByUserId($user_id);
-		if ( ! $cookbooks || sizeof($cookbooks) === 0) {
+		$cookbooks = Cookbook::where(['user_id' => $user_id])->get();
+		if ( ! $cookbooks) {
 			return response()->json(['errors' => 'Cookbooks not found.'], 404);
 		}
 
@@ -35,19 +53,15 @@ class CookbookController extends Controller
 		return response()->json($data, 201);
 	}
 
-	public function destroy()
+	public function destroy($id)
 	{
-		$deleted = $this->cookbook->deleteCookbook($id);
-		if ( ! $deleted) {
-			return response()->json(['errors' => 'Unable to delete.'], 422);
-		}
-
+		Cookbook::destroy($id);
 		return response()->json(['success' => 'Cookbook deleted.'], 204);
 	}
 
 	public function show($id)
 	{
-		$cookbook = $this->cookbook->find($id);
+		$cookbook = Cookbook::find($id);
 		if ( ! $cookbook) {
 			return response()->json(['errors' => 'Cookbook not found.'], 404);
 		}
@@ -58,35 +72,42 @@ class CookbookController extends Controller
 		return response()->json($data, 201);
 	}
 
-	public function store(Request $request)
+	public function store()
 	{
-		$cookbook_id = $this->cookbook->store($request);
-		if ( ! $cookbook_id) {
+		$cookbook = Cookbook::create([
+			'user_id' => \Input::get('user_id'),
+			'name' => \Input::get('name'),
+			'description' => \Input::get('description'),
+			'is_public' => \Input::get('is_public')
+		]);
+
+		if ( ! $cookbook->id) {
 			return response()->json(['errors' => 'Failed to create cookbook.'], 422);
 		}
 
-		$success = array(
-			'success' => [
-				'message' => 'Cookbook created!',
-				'id' => $cookbook_id
-			]);
+		Contributor::create([
+			'user_id' => \Input::get('user_id'),
+			'cookbook_id' => $cookbook->id,
+			'role_id' => 2 // TODO - is this the right role?
+		]);
 
-		return response()->json($success, 201);
+		return response()->json(['cookbook_id' => $cookbook->id], 201);
 	}
 
-	public function update($id, Request $request)
+	public function update($id)
 	{
-		$cookbook_id = $this->cookbook->find($id, $request);
+		$cookbook = Cookbook::find($id);
+
 		if ( ! $cookbook) {
 			return response()->json(['errors' => 'Cookbook not found.'], 404);
 		}
 
-		$success = array(
-			'success' => [
-				'message' => 'Cookbook created!',
-				'id' => $cookbook_id
-			]);
+		$cookbook->name = \Input::get('name');
+		$cookbook->description = \Input::get('description');
+		$cookbook->is_public = \Input::get('is_public');
+		$cookbook->touch();
+		$cookbook->save();
 
-		return response()->json($success, 204);
+		return response()->json(['cookbook_id' => $id], 204);
 	}
 }
