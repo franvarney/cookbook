2ff16fc6cfcc8ce06f74737a80f8373f4b0583d8
Fran Varney <fran.varn@gmail.com>
2015-08-26 02:38:58 +0000

    Add password to fillable so passwords are stored in the database

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,7 +29,7 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
 	*
 	* @var array
 	*/
-	protected $fillable = ['username', 'first_name', 'last_name', 'email', 'location', 'is_admin'];
+	protected $fillable = ['username', 'password', 'first_name', 'last_name', 'email', 'location', 'is_admin'];
 
 	/**
 	* The attributes excluded from the model's JSON form.
